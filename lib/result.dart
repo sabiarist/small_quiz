ee619f2251d51a632512df6c05a9513118dcf752
Aristide Sabi <sabiarist@gmail.com>
2021-11-29 00:54:58 +0000

    Reset Quiz feature implemented.

--- lib/result.dart
+++ lib/result.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result(this.resultScore, {Key? key}) : super(key: key);
+  const Result(this.resultScore, this.resetHandler, {Key? key}) : super(key: key);
 
   final int resultScore;
+  final Function resetHandler;
 
   String get resultPhrase {
     String resultText;
-    if (resultScore <=8){
+    if (resultScore <= 8) {
       resultText = 'You are awesome and innocent';
-    } else if (resultScore <=12){
+    } else if (resultScore <= 12) {
       resultText = 'Pretty likeable!';
-    } else if (resultScore <= 16){
+    } else if (resultScore <= 16) {
       resultText = 'You are ... strange !';
-    }else {
+    } else {
       resultText = 'You are so bad !';
     }
     return resultText;
@@ -22,13 +23,28 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: const TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: const TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.all(16.0),
+              primary: Colors.blue,
+              textStyle: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            onPressed: () => resetHandler(),
+            child: const Text('Restart Quiz !'),
+          ),
+        ],
       ),
     );
   }
